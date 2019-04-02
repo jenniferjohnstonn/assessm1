aef3bf9e903fbd2cd27bab86e8c28236ce780979
Jennifer Johnston <c3265115@uon.edu.au>
2019-04-02 01:11:35 +0000

    encryption with key

---
+++ assessm1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+	char message[100], ch;
+	int i, key;
+	
+	printf("Enter a message to encrypt: \n");
+	scanf("%s", message);
+	//gets(message);
+	printf("Enter key: \n");
+	scanf("%d", &key);
+	
+	for(i = 0; message[i] != '\0'; ++i) 
+	{
+		ch = message[i];
+		
+		if(ch >= 'a' && ch <= 'z') // change
+		{
+			ch = ch + key;
+			
+			if(ch > 'z')
+			{
+				ch = ch - 'z' + 'a' - 1;
+			}
+			
+			message[i] = ch;
+		}
+		else if(ch >= 'A' && ch <= 'Z')
+		{
+			ch = ch + key;
+			
+			if(ch > 'Z'){
+				ch = ch - 'Z' + 'A' - 1;
+			}
+			
+			message[i] = ch;
+		}
+	}
+	
+	printf("Encrypted message: %s\n", message);
+	
+	return 0;
+}
