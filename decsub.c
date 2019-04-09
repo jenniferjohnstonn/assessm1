c8761be96c3eea487495d61a3d891143435b953b
Jennifer Johnston <c3265115@uon.edu.au>
2019-04-09 11:53:50 +0000

    decryption of substitution cipher w/ key

--- decsub.c
+++ decsub.c
@@ -2,5 +2,37 @@
 
 int main(void)
 {
-   char 
+    char message[80];
+    char emessage[80];
+    char dmessage[80];
+    
+    int key;
+    int temp;
+    
+    printf("Enter the text to decrypt:\n");
+    scanf("%[^\n]s",message);
+    printf("%s\n",message);
+    printf("Enter the decryption key:\n");
+    scanf("%d",&key);
+    
+    for (int i = 0; message[i]!='\0'; i++)
+    {
+        if(message[i]== ' ')
+        {
+            emessage[i] = ' ';
+            printf("%c", emessage[i]);
+        }
+        else
+        {
+
+        temp = message[i];
+        temp = (temp-65-key)%26;
+        emessage[i] = temp + 65;
+        printf("%c", emessage[i]);
+    }
+
+        
+    }
+    printf("\n");
+    return 0;
 }
