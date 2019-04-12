5c92eb7e0f20488a43e47ecf561ab1b9249420c7
Jennifer Johnston <c3265115@uon.edu.au>
2019-04-12 00:33:02 +0000

    trying to add lower case letters

--- new.c
+++ new.c
@@ -1,3 +1,5 @@
+// encryption for substitution cipher 
+
 #include <stdio.h>
 
 int main(void)
@@ -11,7 +13,6 @@ int main(void)
     
     printf("Enter the text to encrypt:\n");
     scanf("%[^\n]s",message);
-    printf("%s\n",message);
     printf("Enter the encryption key:\n");
     scanf("%d",&key);
     
@@ -22,17 +23,25 @@ int main(void)
             emessage[i] = ' ';
             printf("%c", emessage[i]);
         }
-        else
+        
+        else // upper case letters
         {
-
         temp = message[i];
         temp = (temp-65+key)%26;
         emessage[i] = temp + 65;
         printf("%c", emessage[i]);
-    }
-
+        }
         
+        else // lower case letters
+        {
+          temp = message[i];
+        temp = (temp-97+key)%26;
+        emessage[i] = temp + 97;
+        printf("%c", emessage[i]);   
+        }
+    
+    }        
     }
-    printf("\n");
+    printf("Encrpted message: \n");
     return 0;
 }
